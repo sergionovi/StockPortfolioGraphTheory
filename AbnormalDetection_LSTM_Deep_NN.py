a4f7d7d94dd269945361db6ea7193a0867f823b0
Novi <sergiolnovi@gmail.com>
2020-06-19 08:34:07 -0300

    Create AbnormalDetection_LSTM_Deep_NN.py
     - Getting in touch with the data

---
+++ AbnormalDetection_LSTM_Deep_NN.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 19 08:08:18 2020
+Objectives:
+    1 - Remove abnormal with LSTM data from stock time series.
+    The main idea of the project is not only remove outliers, but also 
+    learn and get in touch with LSTM deep Neural Networks. 
+    
+        Required Libraries:           
+            numpy
+            matplotlib
+            pandas
+            keras
+            math
+            
+@author: Sergio Novi, sergiolnovi@gmail.com
+
+"""
+# Importing usefull libraries
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import mean_squared_error
+from keras.layers.core import Dense, Activation, Dropout
+import time #helper libraries
+import scipy.io
+
+
+# Load the previous downloaded data 
+
+data = pd.read_csv('HistoricalData.csv');
+
+
+
+
+
+
